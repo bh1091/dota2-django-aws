ed316f0892e498ea41afb2ebd51b3283a19a21f7
bh1091 <bh1091@nyu.edu>
2014-04-16 17:20:59 -0400

    configure admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,3 +85,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
