e929a47a7fe05fa1137d96b0984e0d77c66d78a1
bh1091 <bh1091@nyu.edu>
2014-04-17 17:32:38 -0400

    search alpha 0.2

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,22 +58,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #         'ENGINE': 'django.db.backends.mysql',
-    #         'NAME': os.environ['RDS_DB_NAME'],
-    #         'USER': os.environ['RDS_USERNAME'],
-    #         'PASSWORD': os.environ['RDS_PASSWORD'],
-    #         'HOST': os.environ['RDS_HOSTNAME'],
-    #         'PORT': os.environ['RDS_PORT'],
-    #     }
     'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': '',
-            'USER': '',
-            'PASSWORD': '',
-            'HOST': '',
-            'PORT': '',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         }
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.mysql',
+    #         'NAME': '',
+    #         'USER': '',
+    #         'PASSWORD': '',
+    #         'HOST': '',
+    #         'PORT': '',
+    #     }
 }
 
 # Internationalization
