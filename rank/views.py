80cf90857ba73d14579f22e57446139d5457647d
bh1091 <bh1091@nyu.edu>
2014-05-09 07:01:59 -0400

    almost done

--- rank/views.py
+++ rank/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render_to_response
 import os
+import urllib
+
 
 def rank(request):
-	module_dir = os.path.dirname(__file__)  # get current directory
-	file_path = os.path.join(module_dir, 'apr.txt')
-	apr_file = open(file_path,'r')
+	# module_dir = os.path.dirname(__file__)  # get current directory
+	# file_path = os.path.join(module_dir, 'apr.txt')
+	# apr_file = open('http://s3.amazonaws.com/match-history/apr.txt','r')
+
+	opener = urllib.URLopener()
+	apr_file = opener.open('http://s3.amazonaws.com/match-history/apr.txt')
 	aprs = apr_file.readlines()
 
 	hero_list = []
